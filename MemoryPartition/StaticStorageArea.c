e77d147f4b730868ef50dce052f91702c84e1a65
RainbowXie <cmdweilan@yahoo.com>
2017-12-17 12:23:58 +0800

    MemoryPartition

---
+++ MemoryPartition/StaticStorageArea.c
@@ -0,0 +1,31 @@
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+
+char* getStr1()
+{
+	char* p = "abcdefg";
+	return p;
+}
+
+char* getStr2()
+{
+	char* p2 = "abcdefg2";
+	return p2;
+}
+
+
+
+int main()
+{
+	char* p1 = NULL;
+	char* p2 = NULL;
+
+	p1 = getStr1();
+	p2 = getStr2();
+
+	printf("p1:%s, p2:%s\n", p1, p2);
+	printf("p1:%p, p2:%p\n", p1, p2);
+	
+	return 0;
+}
